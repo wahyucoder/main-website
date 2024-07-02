ae52c2917c036eafc9d425d7c72ed92be568f119
wahyu coder <wahyunurhadi.coder@gmail.com>
2024-07-02 15:14:35 +0700

    code vuejs tailwind

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
